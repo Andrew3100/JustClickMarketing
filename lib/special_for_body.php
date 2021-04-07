c406d59a5b2722bb3572aba59de39e0f3495b588
Andrey <funikov.1997@mail.ru>
2021-04-08 01:27:26 +0300

    Добавил некоторые макеты

--- lib/special_for_body.php
+++ lib/special_for_body.php
@@ -35,6 +35,15 @@
                 <a href="index.php?notes=1"><p style="color: white">Задачи</p></a>
             </div><br>
             <div class="row">
+                <a href="index.php?context=1"><p style="color: white">Контекстная реклама</p></a>
+            </div><br>
+            <div class="row">
+                <a href="index.php?target=1"><p style="color: white">Таргетинговая реклама</p></a>
+            </div><br>
+            <div class="row">
+                <a href="index.php?social=1"><p style="color: white">Социальные сети</p></a>
+            </div><br>
+            <div class="row">
                 <a href="index.php?courses=1"><p style="color: white">Курсы</p></a>
             </div><br>
             <div class="row">
@@ -81,6 +90,16 @@ function GetLinkName() {
     if ($_GET['partners']) {
         $get_file_name = 'partners';
     }
+    if ($_GET['context']) {
+        $get_file_name = 'context';
+    }
+    if ($_GET['target']) {
+        $get_file_name = 'target';
+    }
+    if ($_GET['social']) {
+        $get_file_name = 'social';
+    }
+
     /*Выполняем тот файл, на который указывает параметр в адресной строке*/
     return include "tabs/$get_file_name.php";
 }
--- tabs/partners.php
+++ tabs/partners.php
@@ -1,2 +1,45 @@
 <?php
-echo 'партнёрка';
+
+
+echo '<h4>Подключено партнёров: 1</h4><br>Ссылка на страницу регистрации партнера: <a href="https://vlyagusha.justclick.ru/join/">https://vlyagusha.justclick.ru/join/</a><br>
+Ссылка для входа в личный кабинет партнера: <a href="https://vlyagusha.justclick.ru/partner/">https://vlyagusha.justclick.ru/partner/</a>';
+
+
+echo '<br><br>
+<div class="container">
+<div class="row">
+<div class="col">
+<table class="table table-dark bordered">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Логин</th>
+      <th scope="col">Заработано</th>
+      <th scope="col">Выплатить</th>
+      <th scope="col">Кликов</th>
+      <th scope="col">Подписчиков</th>
+      <th scope="col">Оплат</th>
+      <th scope="col">Партнёров</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>461249</td>
+      <td>partner-lyagusha-0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+      <td>0</td>
+    </tr>
+    
+    
+  </tbody>
+</table>
+<button class="btn btn-success">Добавить партнёра</button>
+</div>
+</div>
+</div>
+
+';
