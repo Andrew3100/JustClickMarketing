f0fa5cde126a3362d1351fff7880480a3fd58470
Фуников Андрей Дмитриевич <funikov.1997@mail.ru>
2021-05-12 16:44:57 +0300

    Функционал системы

--- tabs/support_add.php
+++ tabs/support_add.php
@@ -1,6 +1,14 @@
 <?php
-
+include 'scripts/database.php';
  $header = $_POST['header'];
  $description = $_POST['description'];
  $mail = $_POST['mail'];
+$username = $_COOKIE['user'];
+$date = date("Y-m-d",time());
+$time = date("H:i:s",time());
+$add_supp = $mysqli->query("INSERT INTO support_list (username,mail,`date`,`time`) VALUES ('$username','$mail','$date','$time','$header','$description')");
 
+if ($add_supp) {
+    echo 'добавлено';
+}
+print_r("INSERT INTO support_list (username,mail,`date`,`time`) VALUES ('$username','$mail','$date','$time','$header','$description')");
